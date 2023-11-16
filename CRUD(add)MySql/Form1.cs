3643c7429469841e70bcee614aa3cd76eba0b694
limaismael8901 <limaismael8901@gmail.com>
2023-11-16 05:07:58 -0300

    Sucess 5, Field 1

--- CRUD(add)MySql/Form1.cs
+++ CRUD(add)MySql/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿
+
+
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +11,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
+
 
 namespace CRUD_add_MySql
 {
     public partial class Form1 : Form
     {
-        MySqlConnection conn;
-
+        MySqlConnection ConexaoComMySql;
+       
         public Form1()
         {
             InitializeComponent();
@@ -29,28 +33,27 @@ namespace CRUD_add_MySql
         {
             try
             {
-                string sql = "datasource=localhost;username=root;password = 123456789; database = db_agenda";
-                conn = new MySqlConnection(sql);
+                string BDMySql = "datasource=localhost;username=root;password = 123456789; database = db_agenda";
+                ConexaoComMySql = new MySqlConnection(BDMySql);
 
-                string comandSql = "INSERT INTO contato(nome, email, telefone) " +
+                string ComandInsert = "INSERT INTO contato(nome, email, telefone) " +
                     "VALUES('" + txtNome.Text + "','" + txtEmail.Text + "','" + txtTelefone.Text + "')";
-                MySqlCommand comand = new MySqlCommand(comandSql, conn);
+                MySqlCommand comandMysql = new MySqlCommand(ComandInsert, ConexaoComMySql);
 
-                conn.Open();
-                comand.ExecuteReader();
+                ConexaoComMySql.Open();
+                comandMysql.ExecuteReader();
 
                 MessageBox.Show("Sucesso");
-            }catch(Exception ex)
+
+            }
+            catch(Exception ex)
             {
-                MessageBox.Show("Erro");
+                MessageBox.Show(ex.Message);
             }
             finally
             {
-                conn.Close();
+                ConexaoComMySql.Close();
             }
-
-            
-          
         }
     }
 }
